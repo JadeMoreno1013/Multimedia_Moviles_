fe993dfc2a8ec1e8b4fe052552af5ac8ef494802
Jade <jsmorenom1@academia.usbbog.edu.co>
2026-04-22 15:30:24 -0500

    Update lesson_Node.dart

--- PROYECTO_DUOLINGO 2°Corte/flutter_application_1/lib/Widgets/lesson_Node.dart
+++ PROYECTO_DUOLINGO 2°Corte/flutter_application_1/lib/Widgets/lesson_Node.dart
@@ -1,42 +1,87 @@
 import 'package:flutter/material.dart';
 
-class LessonNode extends StatelessWidget {
-
+class LessonNode extends StatefulWidget {
   final double width;
   final dynamic content;
   final Color color;
+  final VoidCallback? onTap;
 
   const LessonNode({
     super.key,
     required this.width,
     required this.content,
     required this.color,
+    this.onTap,
   });
 
   @override
+  State<LessonNode> createState() => _LessonNodeState();
+}
+
+class _LessonNodeState extends State<LessonNode> {
+  bool _isPressed = false;
+
+  void _onTapDown(_) {
+    setState(() => _isPressed = true);
+  }
+
+  void _onTapUp(_) {
+    setState(() => _isPressed = false);
+  }
+
+  void _onTapCancel() {
+    setState(() => _isPressed = false);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      width: width * 0.18,
-      height: width * 0.18,
-      decoration: BoxDecoration(
-        color: color,
-        shape: BoxShape.circle,
-      ),
-      child: Center(
-        child: content is IconData
-            ? Icon(
-                content,
-                color: Colors.white,
-                size: width * 0.08,
-              )
-            : ClipOval(
-                child: Image.asset(
-                  content,
-                  width: width * 0.10,
-                  height: width * 0.10,
-                  fit: BoxFit.cover,
+    final size = widget.width * 0.18;
+
+    return GestureDetector(
+      onTap: widget.onTap,
+      onTapDown: _onTapDown,
+      onTapUp: _onTapUp,
+      onTapCancel: _onTapCancel,
+
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 100),
+        curve: Curves.easeOut,
+
+        width: size,
+        height: size,
+
+        //Animación boton
+        transform: Matrix4.identity()..scale(_isPressed ? 0.9 : 1.0),
+
+        decoration: BoxDecoration(
+          color: widget.color,
+          shape: BoxShape.circle,
+
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withValues(alpha: 0.2),
+              blurRadius: 8,
+              offset: Offset(0, 4),
+            ),
+          ],
+        ),
+
+        child: Center(
+          child: widget.content is IconData
+              ? Icon(
+                  widget.content,
+                  color: Colors.white,
+                  size: widget.width * 0.08,
+                )
+              : ClipOval(
+                  child: Image.asset(
+                    widget.content,
+                    width: widget.width * 0.10,
+                    height: widget.width * 0.10,
+                    fit: BoxFit.cover,
+                  ),
                 ),
-              ),
+        ),
       ),
     );
   }
